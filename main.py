ae474194e4d3b63236997e7485156a23608f6bf8
UlisesMSosa <ulisesdevsosa@gmail.com>
2026-06-09 15:34:31 -0300

    Crea la configuracion y layout para manejar registro de nuevos usuarios, login, logout y guardado en BD

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+from cs50 import SQL
 from dotenv import load_dotenv
-from flask import Flask, flash, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from helpers import buscar_libros
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import buscar_libros, apology, login_required
 
 
 app = Flask(__name__)
@@ -10,6 +13,8 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+db = SQL("sqlite:///biblioteca.db")
 
 load_dotenv()
 
@@ -24,10 +29,73 @@ def after_request(response):
     return response
 
 
-
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if not username:
+            return apology("No username")
+        if not password:
+            return apology("No password")
+        if password != confirmation:
+            return apology("Passwords don't mach")
+        
+        hash = generate_password_hash(password)
+        print(hash)
+
+        try:
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?);", username, hash)
+            return redirect("/")
+        except ValueError:
+            return apology("Username already in use")
+        
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        session["user_id"] = rows[0]["id"]
+
+        return redirect("/")
+
+    else:
+        return render_template("login.html")
+    
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
